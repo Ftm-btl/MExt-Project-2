843086a8facbcb8dfb845a8a0751d97b0e568512
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2024-05-27 15:26:23 +0300

    EntityBase edit Product

--- Mext-Project-2/Mext-Project-2.Domain/Entities/Product.cs
+++ Mext-Project-2/Mext-Project-2.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mext_Project_2.Domain.Comman;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Mext_Project_2.Domain.Entities
 {
-    public class Product
+    public class Product : EntityBase
     {
-        public Guid Id { get; set; }
+        
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
